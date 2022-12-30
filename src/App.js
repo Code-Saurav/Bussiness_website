4405330d6a3d5a151a6633c22793f4afec74f5d4
Saurav Sah <saurav.sah@mavs.uta.edu>
2022-12-29 19:18:32 -0600

    components ideas

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-//html code inside the jsx function : 
-import ExpenseItem from  './components/ExpenseItem'
+//html code inside the jsx function :
+import Expenses from "./components/Expenses";
+
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    { id: "e2", title: "Auto", amount: 39.67, date: new Date(2019, 2, 28) },
+    { id: "e3", title: "HairCut", amount: 2.67, date: new Date(2023, 4, 28) },
+    { id: "e4", title: "Grocery", amount: 32.67, date: new Date(2022, 2, 28) },
+  ];
+
   return (
-   <div>
-    <h2>
-      Hello world
-    </h2>
-    <ExpenseItem></ExpenseItem>
-   </div>
+    <div>
+      <h2>Hello world</h2>
+      <Expenses items = {expenses}/>
+    </div>
   );
 }
 
