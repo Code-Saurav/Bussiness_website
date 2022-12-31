80d39dda75ec19b5c4d8790ef87f4a27e29167ca
Saurav Sah <saurav.sah@mavs.uta.edu>
2022-12-31 15:50:50 -0600

    adding new expense works

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 //html code inside the jsx function :
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from './components/NewExpense/NewExpense';
+import {useState} from 'react';
+
+const INITIAL_EXPENSES = [
+  {
+    id: "e1",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  { id: "e2", title: "Auto", amount: 39.67, date: new Date(2019, 2, 28) },
+  { id: "e3", title: "HairCut", amount: 2.67, date: new Date(2023, 4, 28) },
+  { id: "e4", title: "Grocery", amount: 32.67, date: new Date(2022, 2, 28) },
+];
 
 const App = (props) => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    { id: "e2", title: "Auto", amount: 39.67, date: new Date(2019, 2, 28) },
-    { id: "e3", title: "HairCut", amount: 2.67, date: new Date(2023, 4, 28) },
-    { id: "e4", title: "Grocery", amount: 32.67, date: new Date(2022, 2, 28) },
-  ];
+
+  const [expenses, setExpenses] = useState(INITIAL_EXPENSES);
+  
 
   const addExpenseHandler = (expense) => {
-    console.log("In App.js");
-    console.log(expense);
+    // setExpenses([expense, ...expenses]);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    });
   }
 
   return (
